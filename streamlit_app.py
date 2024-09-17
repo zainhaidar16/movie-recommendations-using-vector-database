c4f370ff5478a246bde1a54569e707102d56250d
Zain Haidar <86505295+zainhaidar16@users.noreply.github.com>
2024-09-17 16:09:12 +0200

    Add Weaviate and Cohere integration to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
+import os
+import weaviate
 import streamlit as st
+from weaviate.classes.init import Auth
+from weaviate.util import generate_uuid5
+import weaviate.classes as weaviate_classes
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+weaviate_url = os.environ["WEAVIATE_URL"]
+weaviate_api_key = os.environ["WEAVIATE_API_KEY"]
+cohere_api_key = os.environ["COHERE_APIKEY"]
+
+# Connect to Weaviate Cloud
+client = weaviate.connect_to_wcs(
+    cluster_url=weaviate_url,
+    auth_credentials=Auth.api_key(weaviate_api_key),
+    headers={
+        "X-Cohere-Api-Key": cohere_api_key
+    }
 )
+
